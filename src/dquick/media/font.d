79e465295a7e368d2a03d054b29dee221031b6de
Xavier Bigand <xbigand@koalabs-studio.com>
2013-09-02 20:04:11 +0200

    [Font] work on implementation

--- src/dquick/media/font.d
+++ src/dquick/media/font.d
@@ -9,9 +9,18 @@ import dquick.maths.vector2s32;
 
 import std.string;
 
+/**
+* One Font per size
+* kerning requested at runtime
+*
+*
+**/
+
 // TODO The font manager have to find fonts files in system folders
-// The function FT_Open_Face may help to discover faces types (regular, italic, bold,...) registered in a font file
-// http://www.freetype.org/freetype2/docs/reference/ft2-base_interface.html#FT_Open_Face
+// The function FT_Open_Face may help to discover mFaces types (regular, italic, bold,...) registered in a font file
+// http://www.freetype.org/freetype2/docs/reference/ft2-base_intermFace.html#FT_Open_Face
+
+// TODO migrate FT_Library to FontManager (if it share memory)
 
 class FontManager
 {
@@ -67,51 +76,63 @@ FontManager	fontManager;
 struct Font
 {
 public:
-
+	~this()
+	{
+//		FT_Done_Face(mFace);
+//		FT_Done_FreeType(mLibrary);
+	}
 
 private:
 	void	load(string filePath, int size)
 	{
-		FT_Library		library;
 		FT_Error		error;
-		FT_Face			face;
 		size_t			hres = 64;
 /*		FT_Matrix		matrix = {cast(int)((1.0 / hres) * 0x10000L),
 		cast(int)((0.0) * 0x10000L),
 		cast(int)((0.0) * 0x10000L),
 		cast(int)((1.0) * 0x10000L)};*/
 
-		error = FT_Init_FreeType(&library);
+		error = FT_Init_FreeType(&mLibrary);
 		if (error)
-			throw new Exception(format("Failed to initialize FreeType library. Error : %d", error));
-		scope(exit) FT_Done_FreeType(library);
+			throw new Exception(format("Failed to initialize FreeType mLibrary. Error : %d", error));
 
-		error = FT_New_Face(library, filePath.toStringz(), 0, &face);
+		error = FT_New_Face(mLibrary, filePath.toStringz(), 0, &mFace);
 		if (error)
-			throw new Exception(format("Failed to load face. Error : %d", error));
-		scope(exit) FT_Done_Face(face);
+			throw new Exception(format("Failed to load mFace. Error : %d", error));
 
-		error = FT_Select_Charmap(face, FT_Encoding.FT_ENCODING_UNICODE);
+		error = FT_Select_Charmap(mFace, FT_Encoding.FT_ENCODING_UNICODE);
 		if (error)
 			throw new Exception(format("Failed to select charmap. Error : %d", error));
 
-//		error = FT_Set_Char_Size(face, size * 64, 0, 72 * hres, 72);
-		error = FT_Set_Pixel_Sizes(face, 0, size);
+//		error = FT_Set_Char_Size(mFace, size * 64, 0, 72 * hres, 72);
+		error = FT_Set_Pixel_Sizes(mFace, 0, size);
 		if (error)
 			throw new Exception(format("Failed to select charmap. Error : %d", error));
 
-//		FT_Set_Transform(face, &matrix, null);
+//		FT_Set_Transform(mFace, &matrix, null);
 	}
 
-	void	loadGlyph(uint charCode)
+	Glyph	loadGlyph(uint charCode)
 	{
+		Glyph*	glyph;
+
+		glyph = (charCode in mGlyphs);
+		if (glyph !is null)
+			return *glyph;
+
 		// Load glyphs
+		FT_Error				error;
+		FT_Int32				flags = 0;
+        int						ft_bitmap_width = 0;
+        int						ft_bitmap_rows = 0;
+        int						ft_bitmap_pitch = 0;
+        int						ft_glyph_top = 0;
+        int						ft_glyph_left = 0;
 		FT_Glyph				ft_glyph;
 		FT_GlyphSlot			slot;
 		FT_Bitmap				ft_bitmap;
 		FT_UInt					glyph_index;
 		size_t					i, x, y, width, height, depth, w, h;	// TODO replace x,y and width,height per Vector2s32
-		Glyph					glyph;
 		Atlas.Region			region;
 		size_t					missed = 0;
 		FontManager.ImageAtlas	imageAtlas = fontManager.lastAtlas();
@@ -120,377 +141,209 @@ private:
 		height = imageAtlas.image.height;
 		depth  = imageAtlas.image.nbBytesPerPixel;
 
-/*		for (i = 0; i < face.num_glyphs; i++)
+		glyph_index = FT_Get_Char_Index(mFace, charCode);
+		// WARNING: We use texture-atlas depth to guess if user wants
+		//          LCD subpixel rendering
+
+		if (outline_type > 0)
 		{
-			FT_Int32	flags = 0;
-			int			ft_bitmap_width = 0;
-			int			ft_bitmap_rows = 0;
-			int			ft_bitmap_pitch = 0;
-			int			ft_glyph_top = 0;
-			int			ft_glyph_left = 0;
+			flags |= FT_LOAD_NO_BITMAP;
+		}
+		else
+		{
+			flags |= FT_LOAD_RENDER;
+		}
 
-			glyph_index = FT_Get_Char_Index( face, charcodes[i] );
-			// WARNING: We use texture-atlas depth to guess if user wants
-			//          LCD subpixel rendering
+		if (!hinting)
+		{
+			flags |= FT_LOAD_NO_HINTING | FT_LOAD_NO_AUTOHINT;
+		}
+		else
+		{
+			flags |= FT_LOAD_FORCE_AUTOHINT;
+		}
 
-			if( self.outline_type > 0 )
+
+		if (depth == 3)
+		{
+			FT_Library_SetLcdFilter(mLibrary, FT_LcdFilter.FT_LCD_FILTER_LIGHT);
+			flags |= FT_LOAD_TARGET_LCD;
+			if (filtering)
 			{
-				flags |= FT_LOAD_NO_BITMAP;
-			}
-			else
-			{
-				flags |= FT_LOAD_RENDER;
-			}
-
-			if( !self.hinting )
-			{
-				flags |= FT_LOAD_NO_HINTING | FT_LOAD_NO_AUTOHINT;
-			}
-			else
-			{
-				flags |= FT_LOAD_FORCE_AUTOHINT;
-			}
-
-
-			if( depth == 3 )
-			{
-				FT_Library_SetLcdFilter( library, FT_LCD_FILTER_LIGHT );
-				flags |= FT_LOAD_TARGET_LCD;
-				if( self.filtering )
-				{
-					FT_Library_SetLcdFilterWeights( library, self.lcd_weights );
-				}
-			}
-			error = FT_Load_Glyph( face, glyph_index, flags );
-			if( error )
-			{
-				fprintf( stderr, "FT_Error (line %d, code 0x%02x) : %s\n",
-						__LINE__, FT_Errors[error].code, FT_Errors[error].message );
-				FT_Done_Face( face );
-				FT_Done_FreeType( library );
-				return wcslen(charcodes)-i;
-			}
-
-
-			if( self.outline_type == 0 )
-			{
-				slot            = face.glyph;
-				ft_bitmap       = slot.bitmap;
-				ft_bitmap_width = slot.bitmap.width;
-				ft_bitmap_rows  = slot.bitmap.rows;
-				ft_bitmap_pitch = slot.bitmap.pitch;
-				ft_glyph_top    = slot.bitmap_top;
-				ft_glyph_left   = slot.bitmap_left;
-			}
-			else
-			{
-				FT_Stroker stroker;
-				FT_BitmapGlyph ft_bitmap_glyph;
-				error = FT_Stroker_New( library, &stroker );
-				if( error )
-				{
-					fprintf(stderr, "FT_Error (0x%02x) : %s\n",
-							FT_Errors[error].code, FT_Errors[error].message);
-					FT_Done_Face( face );
-					FT_Stroker_Done( stroker );
-					FT_Done_FreeType( library );
-					return 0;
-				}
-				FT_Stroker_Set( stroker,
-							   cast(int)(self.outline_thickness *64),
-							   FT_STROKER_LINECAP_ROUND,
-							   FT_STROKER_LINEJOIN_ROUND,
-							   0);
-				error = FT_Get_Glyph( face.glyph, &ft_glyph);
-				if( error )
-				{
-					fprintf(stderr, "FT_Error (0x%02x) : %s\n",
-							FT_Errors[error].code, FT_Errors[error].message);
-					FT_Done_Face( face );
-					FT_Stroker_Done( stroker );
-					FT_Done_FreeType( library );
-					return 0;
-				}
-
-				if( self.outline_type == 1 )
-				{
-					error = FT_Glyph_Stroke( &ft_glyph, stroker, 1 );
-				}
-				else if ( self.outline_type == 2 )
-				{
-					error = FT_Glyph_StrokeBorder( &ft_glyph, stroker, 0, 1 );
-				}
-				else if ( self.outline_type == 3 )
-				{
-					error = FT_Glyph_StrokeBorder( &ft_glyph, stroker, 1, 1 );
-				}
-				if( error )
-				{
-					fprintf(stderr, "FT_Error (0x%02x) : %s\n",
-							FT_Errors[error].code, FT_Errors[error].message);
-					FT_Done_Face( face );
-					FT_Stroker_Done( stroker );
-					FT_Done_FreeType( library );
-					return 0;
-				}
-
-				if( depth == 1)
-				{
-					error = FT_Glyph_To_Bitmap( &ft_glyph, FT_RENDER_MODE_NORMAL, 0, 1);
-					if( error )
-					{
-						fprintf(stderr, "FT_Error (0x%02x) : %s\n",
-								FT_Errors[error].code, FT_Errors[error].message);
-						FT_Done_Face( face );
-						FT_Stroker_Done( stroker );
-						FT_Done_FreeType( library );
-						return 0;
-					}
-				}
-				else
-				{
-					error = FT_Glyph_To_Bitmap( &ft_glyph, FT_RENDER_MODE_LCD, 0, 1);
-					if( error )
-					{
-						fprintf(stderr, "FT_Error (0x%02x) : %s\n",
-								FT_Errors[error].code, FT_Errors[error].message);
-						FT_Done_Face( face );
-						FT_Stroker_Done( stroker );
-						FT_Done_FreeType( library );
-						return 0;
-					}
-				}
-				ft_bitmap_glyph = cast(FT_BitmapGlyph) ft_glyph;
-				ft_bitmap       = ft_bitmap_glyph.bitmap;
-				ft_bitmap_width = ft_bitmap.width;
-				ft_bitmap_rows  = ft_bitmap.rows;
-				ft_bitmap_pitch = ft_bitmap.pitch;
-				ft_glyph_top    = ft_bitmap_glyph.top;
-				ft_glyph_left   = ft_bitmap_glyph.left;
-				FT_Stroker_Done(stroker);
-			}
-
-
-			// We want each glyph to be separated by at least one black pixel
-			// (for example for shader used in demo-subpixel.c)
-			w = ft_bitmap_width/depth + 1;
-			h = ft_bitmap_rows + 1;
-			region = texture_atlas_get_region( self.atlas, w, h );
-			if ( region.x < 0 )
-			{
-				missed++;
-				fprintf( stderr, "Texture atlas is full (line %d)\n",  __LINE__ );
-				continue;
-			}
-			w = w - 1;
-			h = h - 1;
-			x = region.x;
-			y = region.y;
-			texture_atlas_set_region( self.atlas, x, y, w, h,
-									 ft_bitmap.buffer, ft_bitmap.pitch );
-
-			glyph = texture_glyph_new( );
-			glyph.charcode = charcodes[i];
-			glyph.width    = w;
-			glyph.height   = h;
-			glyph.outline_type = self.outline_type;
-			glyph.outline_thickness = self.outline_thickness;
-			glyph.offset_x = ft_glyph_left;
-			glyph.offset_y = ft_glyph_top;
-			glyph.s0       = x/cast(float)width;
-			glyph.t0       = y/cast(float)height;
-			glyph.s1       = (x + glyph.width)/cast(float)width;
-			glyph.t1       = (y + glyph.height)/cast(float)height;
-
-			// Discard hinting to get advance
-			FT_Load_Glyph( face, glyph_index, FT_LOAD_RENDER | FT_LOAD_NO_HINTING);
-			slot = face.glyph;
-			glyph.advance_x = slot.advance.x/64.0;
-			glyph.advance_y = slot.advance.y/64.0;
-
-			vector_push_back( self.glyphs, &glyph );
-
-			if( self.outline_type > 0 )
-			{
-				FT_Done_Glyph( ft_glyph );
+				FT_Library_SetLcdFilterWeights(mLibrary, lcd_weights.ptr);
 			}
 		}
-		FT_Done_Face( face );
-		FT_Done_FreeType( library );
-		texture_atlas_upload( self.atlas );
-		texture_font_generate_kerning( self );
-		return missed;*/
+		error = FT_Load_Glyph(mFace, glyph_index, flags);
+		if (error)
+		{
+			throw new Exception(format("Failed to load glyph. Error : %d", error));
+			return *glyph;
+		}
+
+		if (outline_type == 0)
+		{
+			slot            = mFace.glyph;
+			ft_bitmap       = slot.bitmap;
+			ft_bitmap_width = slot.bitmap.width;
+			ft_bitmap_rows  = slot.bitmap.rows;
+			ft_bitmap_pitch = slot.bitmap.pitch;
+			ft_glyph_top    = slot.bitmap_top;
+			ft_glyph_left   = slot.bitmap_left;
+		}
+/*		else
+		{
+			FT_Stroker		stroker;
+			FT_BitmapGlyph	ft_bitmap_glyph;
+			error = FT_Stroker_New(mLibrary, &stroker);
+			if (error)
+			{
+				throw new Exception(format("Failed to create stroker. Error : %d", error));
+				return *glyph;
+			}
+			scope(exit) FT_Stroker_Done(stroker);
+			FT_Stroker_Set(stroker,
+						   cast(int)(outline_thickness *64),
+						   FT_Stroker_LineCap.FT_STROKER_LINECAP_ROUND,
+						   FT_Stroker_LineJoin.FT_STROKER_LINEJOIN_ROUND,
+						   0);
+			error = FT_Get_Glyph(mFace.glyph, &ft_glyph);
+			if (error)
+			{
+				throw new Exception(format("Failed to get glyph. Error : %d", error));
+				return *glyph;
+			}
+
+			if (outline_type == 1)
+			{
+				error = FT_Glyph_Stroke(&ft_glyph, stroker, 1);
+			}
+			else if (outline_type == 2)
+			{
+				error = FT_Glyph_StrokeBorder(&ft_glyph, stroker, 0, 1);
+			}
+			else if (outline_type == 3)
+			{
+				error = FT_Glyph_StrokeBorder(&ft_glyph, stroker, 1, 1);
+			}
+			if (error)
+			{
+				throw new Exception(format("Failed to use stroker. Error : %d", error));
+				return *glyph;
+			}
+
+			if (depth == 1)
+			{
+				error = FT_Glyph_To_Bitmap(&ft_glyph, FT_Render_Mode.FT_RENDER_MODE_NORMAL, 0, 1);
+				if (error)
+				{
+					throw new Exception(format("Failed to convert glyph as bitmap. Error : %d", error));
+					return *glyph;
+				}
+			}
+			else
+			{
+				error = FT_Glyph_To_Bitmap(&ft_glyph, FT_Render_Mode.FT_RENDER_MODE_LCD, 0, 1);
+				if (error)
+				{
+					throw new Exception(format("Failed to convert glyph as bitmap. Error : %d", error));
+					return *glyph;
+				}
+			}
+			ft_bitmap_glyph = cast(FT_BitmapGlyph) ft_glyph;
+			ft_bitmap       = ft_bitmap_glyph.bitmap;
+			ft_bitmap_width = ft_bitmap.width;
+			ft_bitmap_rows  = ft_bitmap.rows;
+			ft_bitmap_pitch = ft_bitmap.pitch;
+			ft_glyph_top    = ft_bitmap_glyph.top;
+			ft_glyph_left   = ft_bitmap_glyph.left;
+		}*/
+
+
+		// We want each glyph to be separated by at least one black pixel
+		// (for example for shader used in demo-subpixel.c)
+		w = ft_bitmap_width/depth + 1;
+		h = ft_bitmap_rows + 1;
+		region = imageAtlas.atlas.allocateRegion(w, h);
+		if (region.x < 0)
+		{
+			missed++;
+			throw new Exception("Texture atlas is full. Instanciate a new one isn't supported yet");	// TODO
+//			continue;
+		}
+		w = w - 1;
+		h = h - 1;
+		x = region.x;
+		y = region.y;
+/*		texture_atlas_set_region(atlas, x, y, w, h,
+								 ft_bitmap.buffer, ft_bitmap.pitch);
+*/
+
+		mGlyphs[charCode] = Glyph();
+		glyph = (charCode in mGlyphs);
+
+		with (*glyph)
+		{
+			glyph.width			= w;
+			glyph.height		= h;
+			outline_type		= outline_type;
+			outline_thickness	= outline_thickness;
+			offset_x			= ft_glyph_left;
+			offset_y			= ft_glyph_top;
+	/*		s0					= x / cast(float)width;
+			t0					= y / cast(float)height;
+			s1					= (x + glyph.width) / cast(float)width;
+			t1					= (y + glyph.height) / cast(float)height;*/
+		}
+
+		// Discard hinting to get advance
+		FT_Load_Glyph(mFace, glyph_index, FT_LOAD_RENDER | FT_LOAD_NO_HINTING);
+		slot = mFace.glyph;
+		glyph.advance_x = slot.advance.x/64.0;
+		glyph.advance_y = slot.advance.y/64.0;
+
+		if (outline_type > 0)
+		{
+			FT_Done_Glyph(ft_glyph);
+		}
+
+		return *glyph;
 	}
 
-	/**
-	* Vector of glyphs contained in this font.
-	*/
-//    vector_t * glyphs;
+private:
+	Glyph[uint]	mGlyphs;
 
-    /**
-	* Atlas structure to store glyphs data.
-	*/
-//    texture_atlas_t * atlas;
+	FT_Library	mLibrary;
+	FT_Face		mFace;
 
-    /**
-	* Font filename
-	*/
-//    char * filename;
+    string	filename;
 
-    /**
-	* Font size
-	*/
-//    float size;
+    float	size;
+    int		hinting;
+    int		outline_type;	// (0 = None, 1 = line, 2 = inner, 3 = outer)
+    float	outline_thickness;
+    int		filtering;
+    ubyte	lcd_weights[5];
 
-    /**
-	* Whether to use autohint when rendering font
-	*/
-//    int hinting;
-
-    /**
-	* Outline type (0 = None, 1 = line, 2 = inner, 3 = outer)
-	*/
-//    int outline_type;
-
-    /**
-	* Outline thickness
-	*/
-//    float outline_thickness;
-
-    /** 
-	* Whether to use our own lcd filter.
-	*/
-//    int filtering;
-
-    /**
-	* LCD filter weights
-	*/
-//    unsigned char lcd_weights[5];
-
-    /**
-	* This field is simply used to compute a default line spacing (i.e., the
-	* baseline-to-baseline distance) when writing text with this font. Note
-	* that it usually is larger than the sum of the ascender and descender
-	* taken as absolute values. There is also no guarantee that no glyphs
-	* extend above or below subsequent baselines when using this distance.
-	*/
-//    float height;
-
-    /**
-	* This field is the distance that must be placed between two lines of
-	* text. The baseline-to-baseline distance should be computed as:
-	* ascender - descender + linegap
-	*/
-//    float linegap;
-
-    /**
-	* The ascender is the vertical distance from the horizontal baseline to
-	* the highest 'character' coordinate in a font face. Unfortunately, font
-	* formats define the ascender differently. For some, it represents the
-	* ascent of all capital latin characters (without accents), for others it
-	* is the ascent of the highest accented character, and finally, other
-	* formats define it as being equal to bbox.yMax.
-	*/
-//    float ascender;
-
-    /**
-	* The descender is the vertical distance from the horizontal baseline to
-	* the lowest 'character' coordinate in a font face. Unfortunately, font
-	* formats define the descender differently. For some, it represents the
-	* descent of all capital latin characters (without accents), for others it
-	* is the ascent of the lowest accented character, and finally, other
-	* formats define it as being equal to bbox.yMin. This field is negative
-	* for values below the baseline.
-	*/
-//    float descender;
-
-    /**
-	* The position of the underline line for this face. It is the center of
-	* the underlining stem. Only relevant for scalable formats.
-	*/
-//    float underline_position;
-
-    /**
-	* The thickness of the underline for this face. Only relevant for scalable
-	* formats.
-	*/
-//    float underline_thickness;
-}
-
-private struct Kerning
-{
-    /**
-	* Left character code in the kern pair.
-	*/
-    uint charcode;
-
-    /**
-	* Kerning value (in fractional pixels).
-	*/
-    float kerning;
-
+    float	height;
+    float	linegap;
+    float	ascender;
+    float	descender;
+    float	underline_position;
+    float	underline_thickness;
 }
 
 // http://www.freetype.org/freetype2/docs/tutorial/step2.html
 struct Glyph
 {
-    /**
-	* Wide character this glyph represents
-	*/
-    uint charcode;
-
-    /**
-	* Glyph's width in pixels.
-	*/
-    ushort width;
-
-    /**
-	* Glyph's height in pixels.
-	*/
-    ushort height;
-
-    /**
-	* Glyph's left bearing expressed in integer pixels.
-	*/
-    int offset_x;
-
-    /**
-	* Glyphs's top bearing expressed in integer pixels.
-	*
-	* Remember that this is the distance from the baseline to the top-most
-	* glyph scanline, upwards y coordinates being positive.
-	*/
-    int offset_y;
-
-    /**
-	* For horizontal text layouts, this is the horizontal distance (in
-	* fractional pixels) used to increment the pen position when the glyph is
-	* drawn as part of a string of text.
-	*/
-    float advance_x;
-
-    /**
-	* For vertical text layouts, this is the vertical distance (in fractional
-	* pixels) used to increment the pen position when the glyph is drawn as
-	* part of a string of text.
-	*/
-    float advance_y;
-
-    /**
-	* A vector of kerning pairs relative to this glyph.
-	*/
-    Kerning kerning[];
-
-    /**
-	* Glyph outline type (0 = None, 1 = line, 2 = inner, 3 = outer)
-	*/
-    int outline_type;
-
-    /**
-	* Glyph outline thickness
-	*/
-    float outline_thickness;
-
+//    uint	charcode;
+    uint	width;
+    uint	height;
+    int		offset_x;
+    int		offset_y;
+    float	advance_x;
+    float	advance_y;
+    int		outline_type;
+    float	outline_thickness;
 }
 
 shared static this()
