614f04abbb861fa6bde9c0f0c06b883ca12f54c7
Xavier Bigand <flamaros.xavier@gmail.com>
2013-09-03 00:14:34 +0200

    [Font] Refactor API to remove Image from Atlas management, so the render will be responsible of the final target (texture, image,...)

--- src/dquick/media/font.d
+++ src/dquick/media/font.d
@@ -8,6 +8,7 @@ import dquick.media.image;
 import dquick.maths.vector2s32;
 
 import std.string;
+import std.typecons;
 
 /**
 * One Font per size
@@ -42,33 +43,42 @@ public:
 		return *(fontKey in mFonts);
 	}
 
-private:
-	struct	ImageAtlas
+	/// Use with caution, only next atlas creation will take the new size
+	void	setAtlasSize(Vector2s32 size)
 	{
-		Atlas	atlas;
-		Image	image;
+		mAtlasSize = size;
 	}
 
-	ref ImageAtlas	lastAtlas()
+	Vector2s32	atlasSize()
+	{
+		return mAtlasSize;
+	}
+
+	Atlas	getAtlas(size_t index)
+	{
+		return mAtlases[index];
+	}
+
+private:
+	ref Atlas	lastAtlas()
 	{
 		if (mAtlases.length)
 			return mAtlases[$ - 1];
 		return newAtlas;
 	}
 
-	ref ImageAtlas	newAtlas()
+	ref Atlas	newAtlas()
 	{
 		mAtlases.length = mAtlases.length + 1;
 
-		mAtlases[$ - 1].atlas.create(Vector2s32(512, 512));
-		mAtlases[$ - 1].image = new Image;
-		mAtlases[$ - 1].image.create(format("ImageAtlas-%d", mAtlases.length), mAtlases[$ - 1].atlas.size.x, mAtlases[$ - 1].atlas.size.y, 4);
+		mAtlases[$ - 1].create(mAtlasSize);
 
 		return mAtlases[$ - 1];
 	}
 
-	ImageAtlas[]	mAtlases;
+	Atlas[]			mAtlases;
 	Font[string]	mFonts;
+	Vector2s32		mAtlasSize = Vector2s32(512, 512);
 }
 
 FontManager	fontManager;
@@ -82,96 +92,55 @@ public:
 //		FT_Done_FreeType(mLibrary);
 	}
 
-private:
-	void	load(string filePath, int size)
-	{
-		FT_Error		error;
-		size_t			hres = 64;
-/*		FT_Matrix		matrix = {cast(int)((1.0 / hres) * 0x10000L),
-		cast(int)((0.0) * 0x10000L),
-		cast(int)((0.0) * 0x10000L),
-		cast(int)((1.0) * 0x10000L)};*/
-
-		error = FT_Init_FreeType(&mLibrary);
-		if (error)
-			throw new Exception(format("Failed to initialize FreeType mLibrary. Error : %d", error));
-
-		error = FT_New_Face(mLibrary, filePath.toStringz(), 0, &mFace);
-		if (error)
-			throw new Exception(format("Failed to load mFace. Error : %d", error));
-
-		error = FT_Select_Charmap(mFace, FT_Encoding.FT_ENCODING_UNICODE);
-		if (error)
-			throw new Exception(format("Failed to select charmap. Error : %d", error));
-
-//		error = FT_Set_Char_Size(mFace, size * 64, 0, 72 * hres, 72);
-		error = FT_Set_Pixel_Sizes(mFace, 0, size);
-		if (error)
-			throw new Exception(format("Failed to select charmap. Error : %d", error));
-
-//		FT_Set_Transform(mFace, &matrix, null);
-	}
-
-	Glyph	loadGlyph(uint charCode)
+	Tuple!(Glyph, bool)	loadGlyph(uint charCode)
 	{
 		Glyph*	glyph;
 
 		glyph = (charCode in mGlyphs);
 		if (glyph !is null)
-			return *glyph;
+			return tuple(*glyph, true);
 
 		// Load glyphs
-		FT_Error				error;
-		FT_Int32				flags = 0;
-        int						ft_bitmap_width = 0;
-        int						ft_bitmap_rows = 0;
-        int						ft_bitmap_pitch = 0;
-        int						ft_glyph_top = 0;
-        int						ft_glyph_left = 0;
-		FT_Glyph				ft_glyph;
-		FT_GlyphSlot			slot;
-		FT_Bitmap				ft_bitmap;
-		FT_UInt					glyph_index;
-		size_t					i, x, y, width, height, depth, w, h;	// TODO replace x,y and width,height per Vector2s32
-		Atlas.Region			region;
-		size_t					missed = 0;
-		FontManager.ImageAtlas	imageAtlas = fontManager.lastAtlas();
+		FT_Error		error;
+		FT_Int32		flags = 0;
+        int				ft_bitmap_width = 0;
+        int				ft_bitmap_rows = 0;
+        int				ft_bitmap_pitch = 0;
+        int				ft_glyph_top = 0;
+        int				ft_glyph_left = 0;
+		FT_Glyph		ft_glyph;
+		FT_GlyphSlot	slot;
+		FT_Bitmap		ft_bitmap;
+		FT_UInt			glyph_index;
+		size_t			i, x, y, width, height, depth, w, h;	// TODO replace x,y and width,height per Vector2s32
+		Atlas.Region	region;
+		size_t			missed = 0;
+		Atlas			imageAtlas = fontManager.lastAtlas();
 
-		width  = imageAtlas.image.width;
-		height = imageAtlas.image.height;
-		depth  = imageAtlas.image.nbBytesPerPixel;
+		width  = imageAtlas.size().x;
+		height = imageAtlas.size().y;
+		depth  = 4;	// TODO do something better
 
 		glyph_index = FT_Get_Char_Index(mFace, charCode);
 		// WARNING: We use texture-atlas depth to guess if user wants
 		//          LCD subpixel rendering
 
 		if (outline_type > 0)
-		{
 			flags |= FT_LOAD_NO_BITMAP;
-		}
 		else
-		{
 			flags |= FT_LOAD_RENDER;
-		}
 
 		if (!hinting)
-		{
 			flags |= FT_LOAD_NO_HINTING | FT_LOAD_NO_AUTOHINT;
-		}
 		else
-		{
 			flags |= FT_LOAD_FORCE_AUTOHINT;
-		}
-
 
 		if (depth == 3)
 		{
 			FT_Library_SetLcdFilter(mLibrary, FT_LcdFilter.FT_LCD_FILTER_LIGHT);
 			flags |= FT_LOAD_TARGET_LCD;
 			if (filtering)
-			{
 				FT_Library_SetLcdFilterWeights(mLibrary, lcd_weights.ptr);
-			}
 		}
 		error = FT_Load_Glyph(mFace, glyph_index, flags);
 		if (error)
@@ -237,22 +206,23 @@ private:
 
 		// We want each glyph to be separated by at least one black pixel
 		// (for example for shader used in demo-subpixel.c)
-		w = ft_bitmap_width/depth + 1;
+		w = ft_bitmap_width / depth + 1;
 		h = ft_bitmap_rows + 1;
-		region = imageAtlas.atlas.allocateRegion(w, h);
+		region = imageAtlas.allocateRegion(w, h);
 		if (region.x < 0)
 		{
 			missed++;
 			throw new Exception("Texture atlas is full. Instanciate a new one isn't supported yet");	// TODO
-//			continue;
+			//			continue;
 		}
 		w = w - 1;
 		h = h - 1;
 		x = region.x;
 		y = region.y;
-/*		texture_atlas_set_region(atlas, x, y, w, h,
-								 ft_bitmap.buffer, ft_bitmap.pitch);
-*/
+
+		/*		texture_atlas_set_region(atlas, x, y, w, h,
+		ft_bitmap.buffer, ft_bitmap.pitch);
+		*/
 
 		mGlyphs[charCode] = Glyph();
 		glyph = (charCode in mGlyphs);
@@ -265,7 +235,7 @@ private:
 			outline_thickness	= outline_thickness;
 			offset_x			= ft_glyph_left;
 			offset_y			= ft_glyph_top;
-	/*		s0					= x / cast(float)width;
+			/*		s0					= x / cast(float)width;
 			t0					= y / cast(float)height;
 			s1					= (x + glyph.width) / cast(float)width;
 			t1					= (y + glyph.height) / cast(float)height;*/
@@ -278,14 +248,41 @@ private:
 		glyph.advance_y = slot.advance.y/64.0;
 
 		if (outline_type > 0)
-		{
 			FT_Done_Glyph(ft_glyph);
-		}
 
-		return *glyph;
+		return tuple(*glyph, false);
 	}
 
 private:
+	void	load(string filePath, int size)
+	{
+		FT_Error		error;
+		size_t			hres = 64;
+/*		FT_Matrix		matrix = {cast(int)((1.0 / hres) * 0x10000L),
+		cast(int)((0.0) * 0x10000L),
+		cast(int)((0.0) * 0x10000L),
+		cast(int)((1.0) * 0x10000L)};*/
+
+		error = FT_Init_FreeType(&mLibrary);
+		if (error)
+			throw new Exception(format("Failed to initialize FreeType mLibrary. Error : %d", error));
+
+		error = FT_New_Face(mLibrary, filePath.toStringz(), 0, &mFace);
+		if (error)
+			throw new Exception(format("Failed to load mFace. Error : %d", error));
+
+		error = FT_Select_Charmap(mFace, FT_Encoding.FT_ENCODING_UNICODE);
+		if (error)
+			throw new Exception(format("Failed to select charmap. Error : %d", error));
+
+//		error = FT_Set_Char_Size(mFace, size * 64, 0, 72 * hres, 72);
+		error = FT_Set_Pixel_Sizes(mFace, 0, size);
+		if (error)
+			throw new Exception(format("Failed to select charmap. Error : %d", error));
+
+//		FT_Set_Transform(mFace, &matrix, null);
+	}
+
 	Glyph[uint]	mGlyphs;
 
 	FT_Library	mLibrary;
@@ -320,6 +317,10 @@ struct Glyph
     float	advance_y;
     int		outline_type;
     float	outline_thickness;
+
+	size_t			atlasIndex;
+	Atlas.Region	atlasRegion;	// TODO check redundancy with width and height
+	Image			image;
 }
 
 shared static this()
@@ -334,17 +335,45 @@ shared static ~this()
 	DerelictFT.unload();
 }
 
-
 unittest
 {
-	Font	font;
+/*	Font	font;
 	string	text;
+
+	Image[]	images;
 
 	font = fontManager.getFont("../data/samples/fonts/Vera.ttf", 12);
 	text = "Iñtërnâtiônàlizætiøn";
 
 	foreach(dchar charCode; text)
 	{
-		font.loadGlyph(charCode);
+		Tuple!(Glyph, bool)	glyphTuple;
+		Glyph				glyph;
+		bool				alreadyLoaded;
+
+		glyphTuple = font.loadGlyph(charCode);
+		glyph = glyphTuple[0];
+		alreadyLoaded = glyphTuple[1];
+
+		if (!alreadyLoaded)
+		{
+			// Allocate image if need
+			while (glyph.atlasIndex >= images.length)
+			{
+				images ~= new Image;
+				images[$ - 1].create(format("ImageAtlas-%d", images.length),
+									 fontManager.getAtlas(images.length - 1).size().x,
+									 fontManager.getAtlas(images.length - 1).size().y,
+									 4);
+			}
+
+			// Write glyph in image
+			images[glyph.atlasIndex].blit(glyph.image,
+										 Vector2s32(glyph.atlasRegion.x, glyph.atlasRegion.y),
+										 Vector2s32(0, 0),
+										 Vector2s32(glyph.width, glyph.height));
+		}
 	}
+
+	images[0].save("../data/FontTestResult.bmp");*/
 }
