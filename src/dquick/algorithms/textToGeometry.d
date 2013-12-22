6d19fc2d27807c9a22f56bd20e455932a0fdb74e
Flamaros <flamaros.xavier@gmail.com>
2013-12-22 16:41:24 +0100

    Rmove unessary source

--- src/dquick/algorithms/textToGeometry.d
+++
@@ -1,8 +0,0 @@
-module dquick.algorithms.text_to_geometry;
-
-// TODO Write methodes as static
-
-/// This module provide function to convert string or a markuped text to a Geometry object
-class TextToGeometry
-{
-}
